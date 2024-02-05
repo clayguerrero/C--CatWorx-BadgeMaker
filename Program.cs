2a616f6f38324b1879fdf1d8c4c47780fabdaa8b
clayg <claytondguerrero@gmail.com>
2024-02-04 18:25:40 -0600

    inital push

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                // Add currentEmployee, not a string
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(
+                    string.Format(
+                        template,
+                        employees[i].GetId(),
+                        employees[i].GetFullName(),
+                        employees[i].GetPhotoUrl()
+                    )
+                );
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+    }
+}
