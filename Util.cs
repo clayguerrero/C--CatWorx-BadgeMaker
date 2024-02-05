e63f897adfa9a9436f062967d3e12ca41de92d45
clayg <claytondguerrero@gmail.com>
2024-02-04 19:08:06 -0600

    MakeCSV now runs properly

--- Util.cs
+++ Util.cs
@@ -30,6 +30,26 @@ namespace CatWorx.BadgeMaker
                 // If not, create it
                 Directory.CreateDirectory("data");
             }
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                // code that needs StreamWriter here
+                file.WriteLine("ID,Name,PhotoUrl");
+
+                // Loop over employees
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    // Write each employee to the file
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(
+                        String.Format(
+                            template,
+                            employees[i].GetId(),
+                            employees[i].GetFullName(),
+                            employees[i].GetPhotoUrl()
+                        )
+                    );
+                }
+            }
         }
     }
 }
